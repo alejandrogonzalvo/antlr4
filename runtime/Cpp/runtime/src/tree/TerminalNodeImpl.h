a894bd302b96f7964111f24fa9d29d0bc888814b
alejandrogonzalvo <alejandrogonhid@protonmail.com>
2024-02-14 11:54:34 +0100

    add equals function

--- runtime/Cpp/runtime/src/tree/TerminalNodeImpl.h
+++ runtime/Cpp/runtime/src/tree/TerminalNodeImpl.h
@@ -26,6 +26,15 @@ namespace tree {
     virtual std::string toStringTree(Parser *parser, bool pretty = false) override;
     virtual std::string toString() override;
     virtual std::string toStringTree(bool pretty = false) override;
+
+    inline bool operator== (const TerminalNode &other) const {
+      std::cout << "TerminalNodeImpl operator== called" << std::endl;
+      return getSymbol()->getText() == other.getSymbol()->getText() && getSymbol()->getType() == other.getSymbol()->getType();
+    }
+
+    inline bool equals(const TerminalNode* other) const {
+      return getSymbol()->getText() == other->getSymbol()->getText() && getSymbol()->getType() == other->getSymbol()->getType();
+    }
   };
 
 } // namespace tree
